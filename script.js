fd1485b728d59ee2c76809655d475449df81772e
Brandon <Bdzrox01@gmail.com>
2022-01-16 19:26:14 -0500

    Made function that plays a single round of RPS.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+/*  
+*    Function that returns Rock, Paper, or Scissor randomly
+*    This function is made by using Math.random()
+*/ 
+
 let computer = function() {
     
     // storing random number 1-3 in var num
@@ -14,4 +20,37 @@ let computer = function() {
   
 }
 
-console.log(computer());
+/*
+    Function that will play a single round of Rock-Paper-Scissors
+    The main logic will be based around if else statements.
+    This function takes input from the player and computer.
+*/
+let round = function(playerSelection, computerSelection) {
+
+    // main logic that checks to see which which player wins, using else if statements. 
+
+    if (playerSelection == computerSelection) {
+        return "It's a Tie!";
+
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    } else {
+        return `You lose ${computerSelection} beats ${playerSelection}`;
+    }
+
+    
+}
+
+
+
+
+
+
+//console.log(computer());
