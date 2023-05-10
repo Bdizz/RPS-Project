dbc035387105cd93793183a14312e3e6595fac87
Brandon <bdzrox01@gmail.com>
2023-05-10 01:57:45 -0400

    made changes to the code to make it more clear and modern.

--- script.js
+++ script.js
@@ -1,100 +1,73 @@
+// Define the game rules using an object where each option is associated with its defeating option
+const gameRules = {
+  Rock: "Scissors",
+  Paper: "Rock",
+  Scissors: "Paper",
+};
 
-/*  
-*    Function that returns Rock, Paper, or Scissor randomly
-*    This function is made by using Math.random()
-*/ 
+// Function to generate a random computer selection using the game rules
+const computer = () => {
+  let options = Object.keys(gameRules);
+  let randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+};
 
+// Function to determine the result of a round based on player and computer selections
+const round = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "It's a Tie!";
+  } else if (gameRules[playerSelection] === computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+};
 
-let computer = function() {
-    
-    // storing random number 1-3 in var num
-    let num = Math.floor((Math.random() * 3) + 1);
+// Function to prompt the player for their selection, validate it, and return the choice
+const getPlayerSelection = () => {
+  let validOptions = Object.keys(gameRules);
+  let userChoice = prompt(
+    `Type either ${validOptions.join(", ")} in order to start the game.`
+  );
+  while (!validOptions.includes(userChoice)) {
+    userChoice = prompt(
+      `Invalid input. Type either ${validOptions.join(", ")} to start the game.`
+    );
+  }
+  return userChoice;
+};
 
-    // based on number return string (either R, P, S)
-    if (num == 1) {
-        return "Rock";
-    } else if (num == 2) {
-        return "Paper";
+// Function that manages the game logic, keeps track of points, and determines the winner
+const gameLogic = () => {
+  let playerPoints = 0;
+  let computerPoints = 0;
+
+  // Play rounds until either the player or the computer reaches 5 points
+  while (playerPoints < 5 && computerPoints < 5) {
+    let userChoice = getPlayerSelection();
+    let result = round(userChoice, computer());
+
+    console.log(result);
+    if (result === "It's a Tie!") {
+      console.log(`Score: ${playerPoints} - ${computerPoints}`);
+    } else if (result.startsWith("You Win!")) {
+      playerPoints += 1;
+      console.log(`Score: ${playerPoints} - ${computerPoints}`);
     } else {
-        return "Scissors";
+      computerPoints += 1;
+      console.log(`Score: ${playerPoints} - ${computerPoints}`);
     }
-  
-}
+  }
 
-/*
-    Function that will play a single round of Rock-Paper-Scissors
-    The main logic will be based around if else statements.
-    This function takes input from the player and computer.
-*/
-let round = function(playerSelection, computerSelection) {
+  // Determine the winner based on the points and display the result
+  if (playerPoints === 5) {
+    console.log("Game over, you Win!");
+    return "Game over! You Win!";
+  } else {
+    console.log("Game over, you Lose!");
+    return "Game over! You lose!";
+  }
+};
 
-    // main logic that checks to see which which player wins, using else if statements. 
-
-    if (playerSelection == computerSelection) {
-        return "It's a Tie!";
-
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-
-    } else {
-        return `You lose ${computerSelection} beats ${playerSelection}`;
-    }
-
-    
-}
-
-
-let gameLogic = function() {
-
-    let playerPoints = 0, computerPoints = 0;
-    
-
-    while (playerPoints < 5 && computerPoints < 5) {
-
-        let userChoice = prompt('Type either "Rock", "Paper", "Scissors" in order to start the game.');
-
-        let result = round(userChoice, computer());
-
-        if (result == "It's a Tie!") {
-            console.log(result);
-            console.log(`Score: ${playerPoints} - ${computerPoints}`);
-
-        } else if (result == "You Win! Rock beats Scissors") {
-            console.log(result);
-            playerPoints += 1;
-            console.log(`Score: ${playerPoints} - ${computerPoints}`);
-
-        } else if (result == "You Win! Paper beats Rock") {
-            console.log(result);
-            playerPoints += 1;
-            console.log(`Score: ${playerPoints} - ${computerPoints}`);
-
-        } else if (result == "You Win! Scissors beats Paper") {
-            console.log(result);
-            playerPoints += 1;
-            console.log(`Score: ${playerPoints} - ${computerPoints}`);
-
-        } else {
-            console.log(result);
-            computerPoints += 1;
-            console.log(`Score: ${playerPoints} - ${computerPoints}`);
-        }
-    }
-
-    if (playerPoints == 5) {
-        console.log("Game over, you Win!");
-        return "Game over! You Win!";
-    } else {
-        console.log("Game over, you Lose!");
-        return "Game over! You lose!";
-    }
-
-}
-
+// Start the game by calling the gameLogic function
 gameLogic();
